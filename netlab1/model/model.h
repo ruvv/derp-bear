2abfba9eddccc5b591b5a6af65b85671e6368fa9
Pecherkin Eugene <e.pecherkin@gmail.com>
2013-04-09 20:22:58 +0400

    Updated model with required vars

--- netlab1/model/model.h
+++ netlab1/model/model.h
@@ -3,11 +3,21 @@
 
 #include <QObject>
 #include <QString>
+#include <QDateTime>
+#include <QUrl>
+
+typedef enum Type {
+    Q1, Q3, Q4, Q8, Q9
+} Type;
 
 class Model : public QObject {
     Q_OBJECT
 
 public:
+    Type type;
+    QDateTime dateTime;
+    QUrl url;
+
     explicit Model(QObject* parent = 0);
 
     virtual QString serialize() = 0;
